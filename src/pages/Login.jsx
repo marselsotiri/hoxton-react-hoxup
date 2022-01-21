f6387b4c5f59b4d3d72581b1f44035a73439524d
Marsel <you@example.com>
2022-01-21 09:27:09 +0100

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react"
 
 function Login({ user, logIn }) {
-  
+
     return <div className="main-wrapper login">
         <section className="login-section">
             <h2>Choose your user!</h2>
             <ul>
                 {user.map(user => <li key={user.id}>
-                    <button className="user-selection" onClick={()=>logIn(user)}>
+                    <button className="user-selection" onClick={() => logIn(user)}>
                         <img
                             className="avatar"
                             width="50"
                             height="50"
-                            src = {user.avatar}
+                            src={user.avatar}
                             alt={`${user.firstName} ${user.lastName}`}
                         />
                         <h3>{user.firstName} {user.lastName}</h3>
                     </button>
                 </li>
-)}
-                
+                )}
+
                 <li>
                     <button className="user-selection"><h3>+ Add a new user</h3></button>
                 </li>
